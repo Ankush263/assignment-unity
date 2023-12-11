f297c23f545ee68bcb5ecce74ad9c023c07e66e7
Ankush263 <ankushbanik263@gmail.com>
2023-12-11 21:44:50 +0530

    Add the routes and global error handler

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const morgan = require('morgan');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const dotenv = require('dotenv');
+const globalErrorHandler = require('./controllers/errorControllers');
+const AppError = require('./utils/appError');
+const cookieParser = require('cookie-parser');
+
+const authRouter = require('./routes/authRoute');
+const buyerRouter = require('./routes/buyerRoutes');
+const sellerRouter = require('./routes/sellerRoutes');
+const productRouter = require('./routes/productRoutes');
+
 dotenv.config();
 
 const app = express();
@@ -16,5 +26,17 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(mongoSanitize());
 app.use(xss());
+app.use(cookieParser());
+
+app.use('/api/auth', authRouter);
+app.use('/api/buyer', buyerRouter);
+app.use('/api/seller', sellerRouter);
+app.use('/api/product', productRouter);
+
+app.all('*', (req, res, next) => {
+	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
