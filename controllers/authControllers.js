273b02745eb35bb3936a64de409cf9adcaed29cb
Ankush263 <ankushbanik263@gmail.com>
2023-12-11 15:17:15 +0530

    Add authControllers

---
+++ controllers/authControllers.js
@@ -0,0 +1,94 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('../model/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
+};
+
+const createAndSendToken = (user, statusCode, res) => {
+	const token = signToken(user._id);
+	const cookieOptions = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+		),
+		httpOnly: true,
+	};
+
+	if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+	res.cookie('jwt', token, cookieOptions);
+	user.password = undefined;
+
+	res.status(statusCode).json({
+		status: 'success',
+		token,
+		data: {
+			user,
+		},
+	});
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+	const { username, email, password, passwordConfirm, role } = req.body;
+
+	const newUser = await User.create({
+		username,
+		email,
+		password,
+		passwordConfirm,
+		role,
+	});
+
+	createAndSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	if (!email || !password) {
+		return next(new AppError(`Please provide email and password`, 404));
+	}
+
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user || !(await user.correctPassword(password, user.password))) {
+		return next(new AppError(`Incorrect email or password`, 401));
+	}
+
+	createAndSendToken(user, 201, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+	let token;
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		token = req.headers.authorization.split(' ')[1];
+	} else if (req.cookies.jwt) {
+		token = req.cookies.jwt;
+	}
+	if (!token) {
+		return next(
+			new AppError(`You aren't logged in! Please log in to get access`, 401)
+		);
+	}
+
+	const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+	const freshUser = await User.findById(decoded.id);
+
+	if (!freshUser) {
+		return next(
+			new AppError(
+				`The user belonging to this token does no longer exists`,
+				401
+			)
+		);
+	}
+	req.user = freshUser;
+	next();
+});
