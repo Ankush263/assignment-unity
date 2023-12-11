3781dbfb73acd28a0721725a8f125323b9da0f5f
Ankush263 <ankushbanik263@gmail.com>
2023-12-11 21:46:44 +0530

    Add new routes to buyer routes

--- routes/buyerRoutes.js
+++ routes/buyerRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
-const { listOfSellers } = require('../controllers/buyerControllers');
+const {
+	listOfSellers,
+	getCatalogBySellerId,
+	createOrder,
+} = require('../controllers/buyerControllers');
 
 const { protect } = require('../controllers/authControllers');
 
 const router = express.Router();
 
 router.use(protect);
+router.route('/seller-catalog/:seller_id').get(getCatalogBySellerId);
+router.route('/create-order/:seller_id').post(createOrder);
 router.route('/list-of-sellers').get(listOfSellers);
 
 module.exports = router;
