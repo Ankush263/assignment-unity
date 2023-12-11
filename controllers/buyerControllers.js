5973f08d866375ee3edf66b6b1e71b550b9968c2
Ankush263 <ankushbanik263@gmail.com>
2023-12-11 21:45:39 +0530

    Add createOrder and getCatalogBySellerId functions

--- controllers/buyerControllers.js
+++ controllers/buyerControllers.js
@@ -1,6 +1,8 @@
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const Catalog = require('../model/catalogModel');
+const Order = require('../model/orderModel');
 
 exports.listOfSellers = catchAsync(async (req, res, next) => {
 	if (req.user.role !== 'buyer') {
@@ -11,6 +13,43 @@ exports.listOfSellers = catchAsync(async (req, res, next) => {
 		status: 'success',
 		data: {
 			sellers,
+		},
+	});
+});
+
+exports.getCatalogBySellerId = catchAsync(async (req, res, next) => {
+	const sellerId = req.params.seller_id;
+	const catalog = await Catalog.find({ user: sellerId });
+	res.status(200).json({
+		status: 'success',
+		data: {
+			catalog,
+		},
+	});
+});
+
+exports.createOrder = catchAsync(async (req, res, next) => {
+	const sellerId = req.params.seller_id;
+	const buyerId = req.user.id;
+
+	const sellerCatalog = await Catalog.find({ user: sellerId });
+	const products = sellerCatalog[0].items.map((item) => item.product._id);
+	const totalAmount = sellerCatalog[0].items.reduce(
+		(sum, item) => sum + item.product.price,
+		0
+	);
+
+	const order = await Order.create({
+		buyer: buyerId,
+		seller: sellerId,
+		products,
+		totalAmount,
+	});
+
+	res.status(201).json({
+		status: 'success',
+		data: {
+			order,
 		},
 	});
 });
